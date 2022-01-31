4982f569858519ffdcf1452efcbd5197b014284f
imangi <imanginanayakkara@gmail.com>
2022-01-31 17:53:01 +0530

    update

--- src/controllers/homepageControllers.js
+++ src/controllers/homepageControllers.js
@@ -1,64 +1,68 @@
-const dotenv = require('dotenv')
+const dotenv = require("dotenv");
 dotenv.config();
 const MY_VERIFY_TOKEN = process.env.MY_VERIFY_TOKEN;
-const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN
-
+const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 
 let getHomepage = (req, res) => {
-    return res.render('homepage.ejs')
+  return res.render("homepage.ejs");
 };
 let getWebhook = (req, res) => {
-    
   // Your verify token. Should be a random string.
-  let VERIFY_TOKEN = MY_VERIFY_TOKEN
-    
+  let VERIFY_TOKEN = MY_VERIFY_TOKEN;
+
   // Parse the query params
-  let mode = req.query['hub.mode'];
-  let token = req.query['hub.verify_token'];
-  let challenge = req.query['hub.challenge'];
-    
+  let mode = req.query["hub.mode"];
+  let token = req.query["hub.verify_token"];
+  let challenge = req.query["hub.challenge"];
+
   // Checks if a token and mode is in the query string of the request
   if (mode && token) {
-  
     // Checks the mode and token sent is correct
-    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-      
+    if (mode === "subscribe" && token === VERIFY_TOKEN) {
       // Responds with the challenge token from the request
-      console.log('WEBHOOK_VERIFIED');
+      console.log("WEBHOOK_VERIFIED");
       res.status(200).send(challenge);
-    
     } else {
       // Responds with '403 Forbidden' if verify tokens do not match
-      res.sendStatus(403);      
+      res.sendStatus(403);
     }
   }
-}
+};
 let postWebhook = (req, res) => {
-    let body = req.body;
+  let body = req.body;
 
   // Checks this is an event from a page subscription
-  if (body.object === 'page') {
-
+  if (body.object === "page") {
     // Iterates over each entry - there may be multiple if batched
-    body.entry.forEach(function(entry) {
-
-      // Gets the message. entry.messaging is an array, but 
-      // will only ever contain one message, so we get index 0
+    body.entry.forEach(function (entry) {
+      // Gets the body of the webhook event
       let webhook_event = entry.messaging[0];
       console.log(webhook_event);
+
+      // Get the sender PSID
+      let sender_psid = webhook_event.sender.id;
+      console.log("Sender PSID: " + sender_psid);
     });
 
     // Returns a '200 OK' response to all requests
-    res.status(200).send('EVENT_RECEIVED');
+    res.status(200).send("EVENT_RECEIVED");
   } else {
     // Returns a '404 Not Found' if event is not from a page subscription
     res.sendStatus(404);
   }
+};
 
-}
+// Handles messages events
+let handleMessage = (sender_psid, received_message) => {};
+
+// Handles messaging_postbacks events
+let handlePostback = (sender_psid, received_postback) => {};
+
+// Sends response messages via the Send API
+let callSendAPI = (sender_psid, response) => {};
 
 module.exports = {
-    getHomepage : getHomepage,
-    getWebhook: getWebhook,
-    postWebhook: postWebhook
-}
+  getHomepage: getHomepage,
+  getWebhook: getWebhook,
+  postWebhook: postWebhook,
+};
