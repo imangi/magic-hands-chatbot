d045dd183b176cfdf7429d1a29778b925413d41d
imangi <imanginanayakkara@gmail.com>
2022-02-26 18:26:47 +0530

    update

--- src/config/viewEngine.js
+++ src/config/viewEngine.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const path = require("path");
-
 /*config view engine*/
 
 let configViewEngine = (app) => {
-  app.use(express.static(path.join(__dirname + "./src/public")));
-
+  app.use(express.static("src/public"));
+  //app.use("/css", express.static(path.join(__dirname + "src/public/css")));
+  app.use("/js", express.static(path.join(__dirname + "src/public/js")));
+  //  app.use("/images", express.static(__dirname + "src/public/images"));
   app.set("viewEngine", "ejs");
-  app.set("views", "./src/views");
+  app.set("views", "src/views");
 };
+
 module.exports = configViewEngine;
--- src/public/js/order.js
+++ src/public/js/order.js
@@ -1,62 +1,41 @@
+/*function validateinputs() {
+  let email = 
+  let orderNUmber = 
+
+}*/
+
 function validateInputFields() {
   const EMAIL_REG =
-    /[a-zA-Z][a-zA-Z0-9_\.]{1,32}@[a-z0-9]{2,}(\.[a-z0-9]{2,4}){1,2}/g;
-  let email = $("#email");
-  let orderNumber = $("#orderNumber");
-  let formEmail = $("#formEmail");
-  let formOrder = $("#formOrder");
+    /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/g;
+
+  let email = document.getElementById("email").value;
+  let orderNumber = document.getElementById("orderNumber").value;
 
   if (!email.val().match(EMAIL_REG)) {
-    formEmail.addClass("was-validated");
+    email.addClass("is-invalid");
     return true;
   } else {
-    formEmail.removeClass("was validated");
+    email.removeClass("is-invalid");
   }
 
   if (orderNumber.val() === "") {
-    formOrder.addClass("was validated");
+    orderNumber.addClass("is-invalid");
     return true;
   } else {
-    formOrder.removeClass("was validated");
+    orderNumber.removeClass("is-invalid");
   }
-
-  return false;
 }
 
 function handleClickButtonFindOrder() {
-  $("#btnFindOrder").on("click", function (e) {
+  document.getElementById("btnFindOrder").on("click", function (e) {
     let check = validateInputFields();
+
     let data = {
       psid: $("#psid").val(),
-      customerName: $("#name").val(),
+      name: $("#name").val(),
       email: $("#email").val(),
       orderNumber: $("#orderNumber").val(),
     };
-
-    if (!check) {
-      //close webview
-      MessengerExtensions.requestCloseBrowser(
-        function success() {
-          // webview closed
-        },
-        function error(err) {
-          // an error occurred
-          console.log(err);
-        }
-      );
-
-      //send data to node.js server
-      $.ajax({
-        url: `${window.location.origin}/set-info-order`,
-        method: "POST",
-        data: data,
-        success: function (data) {
-          console.log(data);
-        },
-        error: function (error) {
-          console.log(error);
-        },
-      });
-    }
+    console.log(check);
   });
 }
